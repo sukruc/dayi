252b67605b15e3a22f7af73e1184255b8ddf6d49
ssezer <sukrucsezer@outlook.com>
2022-07-05 14:04:31 -0700

    yorumcu

--- dayi.py
+++ dayi.py
@@ -2,10 +2,13 @@ import random
 import os
 
 
-data = open("dayi.txt", encoding='utf16').readlines()
+data = open("dayi.txt", encoding='utf-16').readlines()
 
 data = [line for line in data if line.strip()]
 
 soz = random.choice(data)
 
 print(soz)
+
+#
+#
